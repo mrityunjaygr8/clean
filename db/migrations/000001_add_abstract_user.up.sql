db1e6946bdde2790023ee2c63de26de2d03ba61a
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2023-05-23 16:53:27 +0530

    corrected migration files added

--- db/migrations/000001_add_abstract_user.up.sql
+++ db/migrations/000001_add_abstract_user.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS "abstract_users" (
   "id" varchar PRIMARY KEY NOT NULL,
-  "internal_id" serial NOT NULL,
+  "internal_id" serial NOT NULL UNIQUE,
   "email" varchar UNIQUE NOT NULL,
   "password" varchar NOT NULL
 );
--- db/migrations/000002_create_admin_users.up.sql
+++ db/migrations/000002_create_admin_users.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS "admin_users" (
   "id" varchar PRIMARY KEY NOT NULL,
-  "internal_id" serial NOT NULL,
-  "user_id" varchar,
+  "internal_id" serial NOT NULL UNIQUE,
+  "user_id" serial,
   "admin" bool DEFAULT false
 );
 
