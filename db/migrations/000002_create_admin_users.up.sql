f172417ab723fbc784f26cfc394c255bb1ab135c
Mrityunjay Saxena <mrityunjaysaxena1996@gmail.com>
2023-05-23 21:32:03 +0530

    admin in admin_users schema is not null

--- db/migrations/000002_create_admin_users.up.sql
+++ db/migrations/000002_create_admin_users.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "admin_users" (
   "id" varchar PRIMARY KEY NOT NULL,
   "internal_id" serial NOT NULL UNIQUE,
   "user_id" serial,
-  "admin" bool DEFAULT false
+  "admin" bool NOT NULL DEFAULT false
 );
 
 ALTER TABLE "admin_users" ADD CONSTRAINT "fk_admin_users_abstract_users" FOREIGN KEY ("user_id") REFERENCES "abstract_users" ("internal_id");
